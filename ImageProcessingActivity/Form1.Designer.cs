5cb3fe250172662996ce23f31f910ebec269dadd
WendellTamayo <107296776+WendellTamayo@users.noreply.github.com>
2023-11-19 17:31:28 +0800

    Add project files.

---
+++ ImageProcessingActivity/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿using System;
+
+namespace ImageProcessingActivity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.filtersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.greyscaleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorInversionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.histogramToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sepiaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.basicCopyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(30, 48);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(706, 48);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.filtersToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1382, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.importImageToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // importImageToolStripMenuItem
+            // 
+            this.importImageToolStripMenuItem.AccessibleName = "";
+            this.importImageToolStripMenuItem.Name = "importImageToolStripMenuItem";
+            this.importImageToolStripMenuItem.Size = new System.Drawing.Size(183, 26);
+            this.importImageToolStripMenuItem.Text = "Import image";
+            this.importImageToolStripMenuItem.ToolTipText = "Import an image";
+            this.importImageToolStripMenuItem.Click += new System.EventHandler(this.importImageToolStripMenuItem_Click);
+            // 
+            // filtersToolStripMenuItem
+            // 
+            this.filtersToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.greyscaleToolStripMenuItem,
+            this.colorInversionToolStripMenuItem,
+            this.histogramToolStripMenuItem,
+            this.sepiaToolStripMenuItem,
+            this.basicCopyToolStripMenuItem});
+            this.filtersToolStripMenuItem.Name = "filtersToolStripMenuItem";
+            this.filtersToolStripMenuItem.Size = new System.Drawing.Size(62, 24);
+            this.filtersToolStripMenuItem.Text = "Filters";
+            // 
+            // greyscaleToolStripMenuItem
+            // 
+            this.greyscaleToolStripMenuItem.Name = "greyscaleToolStripMenuItem";
+            this.greyscaleToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.greyscaleToolStripMenuItem.Text = "Greyscale";
+            this.greyscaleToolStripMenuItem.Click += new System.EventHandler(this.greyscaleToolStripMenuItem_Click);
+            // 
+            // colorInversionToolStripMenuItem
+            // 
+            this.colorInversionToolStripMenuItem.Name = "colorInversionToolStripMenuItem";
+            this.colorInversionToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.colorInversionToolStripMenuItem.Text = "Color Inversion";
+            this.colorInversionToolStripMenuItem.Click += new System.EventHandler(this.colorInversionToolStripMenuItem_Click);
+            // 
+            // histogramToolStripMenuItem
+            // 
+            this.histogramToolStripMenuItem.Name = "histogramToolStripMenuItem";
+            this.histogramToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.histogramToolStripMenuItem.Text = "Histogram";
+            this.histogramToolStripMenuItem.Click += new System.EventHandler(this.histogramToolStripMenuItem_Click);
+            // 
+            // sepiaToolStripMenuItem
+            // 
+            this.sepiaToolStripMenuItem.Name = "sepiaToolStripMenuItem";
+            this.sepiaToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.sepiaToolStripMenuItem.Text = "Sepia";
+            this.sepiaToolStripMenuItem.Click += new System.EventHandler(this.sepiaToolStripMenuItem_Click);
+            // 
+            // basicCopyToolStripMenuItem
+            // 
+            this.basicCopyToolStripMenuItem.Name = "basicCopyToolStripMenuItem";
+            this.basicCopyToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.basicCopyToolStripMenuItem.Text = "Basic Copy";
+            this.basicCopyToolStripMenuItem.Click += new System.EventHandler(this.basicCopyStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(316, 534);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(95, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Original Image";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(1010, 534);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(115, 22);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "Processed Image";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1382, 603);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem filtersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem greyscaleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorInversionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem histogramToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sepiaToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem basicCopyToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ ImageProcessingActivity/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageProcessingActivity
+{
+    public partial class Form1 : Form
+    {
+        Bitmap loaded;
+        Bitmap processed;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = Properties.Resources.no_image;
+            pictureBox2.Image = Properties.Resources.no_image;
+            pictureBox1.Image.Tag = "default";
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+
+        }
+
+        private void importImageToolStripMenuItem_Click(object sender, EventArgs e)
+
+        {
+            openFileDialog1.Filter = "Image Files|*.bmp;*.jpg;*.jpeg;*.png;*.gif|All Files|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            try
+            {
+                loaded = new Bitmap(openFileDialog1.FileName);
+                pictureBox1.Image = loaded;
+                pictureBox1.Tag = "not_default";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading image: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void basicCopyStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (IsImageLoaded())
+            {
+                try
+                {
+                    loaded = new Bitmap(pictureBox1.Image);
+                    processed = new Bitmap(loaded.Width, loaded.Height);
+
+                    for (int x = 0; x < loaded.Width; x++)
+                    {
+                        for (int y = 0; y < loaded.Height; y++)
+                        {
+                            Color pixel = loaded.GetPixel(x, y);
+                            processed.SetPixel(x, y, pixel);
+                        }
+                    }
+
+                    pictureBox2.Image = processed;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error processing image: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please import an image into pictureBox1 first.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void greyscaleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(IsImageLoaded())
+            {
+                try
+                {
+                    loaded = new Bitmap(pictureBox1.Image);
+                    processed = new Bitmap(loaded.Width, loaded.Height);
+                    Color pixel;
+                    byte gray;
+                    for(int x = 0; x < loaded.Width; x++)
+                    {
+                        for(int y = 0; y < loaded.Height; y++)
+                        {
+                            pixel = loaded.GetPixel(x, y);
+                            gray = (byte)((pixel.R + pixel.G + pixel.B) / 3);
+                            processed.SetPixel(x, y, Color.FromArgb(gray, gray, gray));
+                        }
+                    }
+                    pictureBox2.Image = processed;
+                } catch (Exception ex)
+                {
+                    MessageBox.Show("Error processing image: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            } else
+            {
+                MessageBox.Show("Please import an image into pictureBox1 first.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private bool IsImageLoaded()
+        {
+            return pictureBox1.Image.Tag?.ToString() != "default";
+        }
+
+        private void colorInversionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(IsImageLoaded())
+            {
+                try
+                {
+                    loaded = new Bitmap(pictureBox1.Image);
+                    processed = new Bitmap(loaded.Width, loaded.Height);
+                    Color pixel;
+                    for(int x = 0; x < loaded.Width; x++)
+                    {
+                        for(int y = 0; y < loaded.Height; y++)
+                        {
+                            pixel = loaded.GetPixel(x, y);
+                            processed.SetPixel(x, y, Color.FromArgb(255 - pixel.R, 255 - pixel.G, 255 - pixel.B));
+                        }
+                    }
+                    pictureBox2.Image = processed;
+                } catch (Exception ex)
+                {
+                    MessageBox.Show("Error processing image: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            } else
+            {
+                MessageBox.Show("Please import an image into pictureBox1 first.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void histogramToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (IsImageLoaded())
+            {
+                try
+                {
+                    loaded = new Bitmap(pictureBox1.Image);
+                    processed = new Bitmap(loaded.Width, loaded.Height);
+                    Color pixel;
+                    Byte grayData;
+
+                    // Convert the image to grayscale
+                    for (int x = 0; x < loaded.Width; x++)
+                    {
+                        for (int y = 0; y < loaded.Height; y++)
+                        {
+                            pixel = loaded.GetPixel(x, y);
+                            grayData = (byte)((pixel.R + pixel.G + pixel.B) / 3);
+                            processed.SetPixel(x, y, Color.FromArgb(grayData, grayData, grayData));
+                        }
+                    }
+
+                    // Calculate the histogram data
+                    int[] histdata = new int[256];
+                    for (int x = 0; x < processed.Width; x++)
+                    {
+                        for (int y = 0; y < processed.Height; y++)
+                        {
+                            pixel = processed.GetPixel(x, y);
+                            histdata[pixel.R]++;
+                        }
+                    }
+
+                    // Create a new bitmap for displaying the histogram
+                    Bitmap histogramBitmap = new Bitmap(256, processed.Height);
+
+                    // Clear the bitmap
+                    for (int x = 0; x < histogramBitmap.Width; x++)
+                    {
+                        for (int y = 0; y < histogramBitmap.Height; y++)
+                        {
+                            histogramBitmap.SetPixel(x, y, Color.White);
+                        }
+                    }
+
+                    // Draw the histogram on the bitmap
+                    for (int x = 0; x < 256; x++)
+                    {
+                        for (int y = 0; y < Math.Min(histdata[x] / 5, histogramBitmap.Height - 1); y++)
+                        {
+                            histogramBitmap.SetPixel(x, (histogramBitmap.Height - 1) - y, Color.Black);
+                        }
+                    }
+
+                    // Display the histogram bitmap in pictureBox2
+                    pictureBox2.Image = histogramBitmap;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error processing image: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please import an image into pictureBox1 first.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void sepiaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(IsImageLoaded())
+            {
+                try
+                {
+                    loaded = new Bitmap(pictureBox1.Image);
+                    processed = new Bitmap(loaded.Width, loaded.Height);
+                    Color pixel;
+                    for (int x = 0; x < loaded.Width; x++)
+                    {
+                        for (int y = 0; y < loaded.Height; y++)
+                        {
+                            pixel = loaded.GetPixel(x, y);
+                            int sepiaR = (int)(0.393 * pixel.R + 0.769 * pixel.G + 0.189 * pixel.B);
+                            int sepiaG = (int)(0.349 * pixel.R + 0.686 * pixel.G + 0.168 * pixel.B);
+                            int sepiaB = (int)(0.272 * pixel.R + 0.534 * pixel.G + 0.131 * pixel.B);
+
+                            sepiaR = Math.Min(255, Math.Max(0, sepiaR));
+                            sepiaG = Math.Min(255, Math.Max(0, sepiaG));
+                            sepiaB = Math.Min(255, Math.Max(0, sepiaB));
+                            
+                            processed.SetPixel(x, y, Color.FromArgb(sepiaR, sepiaG, sepiaB));
+                        }
+                    }
+                    pictureBox2.Image = processed;
+                } catch(Exception ex)
+                {
+                    MessageBox.Show("Error processing image: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            } else
+            {
+                MessageBox.Show("Please import an image into pictureBox1 first.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
